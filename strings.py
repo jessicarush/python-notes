52772326a58f40936bebf194c6a292ff3f3314b7
Jessica Rush <info@cyan.red>
2016-09-28 18:13:58 -0700

    Update strings.py

--- strings.py
+++ strings.py
@@ -9,47 +9,64 @@ print(name)
 # Slice with [start : end : step]
 
 letters = 'abcdefghijk'
-print(letters[:])
+print(letters[:-1])
 print(letters[2:])
-print(letters[2:10])
-print(letters[:10])
-print(letters[2:10:2])
+print(letters[2:8])
+print(letters[:8])
+print(letters[2:8:2])
 
-# Split
-test_string = "Yeti, Bigfoot, Loch Ness, Unicorn..."
+# Break a string up into a list with .split()
+
+test_string = "Yeti Bigfoot Loch Ness Unicorn..."
 test_list = test_string.split()
 print(test_list)
 print(type(test_list))
 
-# Join
+# Join a list into a string with .join()
+
 test_string = ' '.join(test_list)
 print(test_string)
 print(type(test_string))
 
-# Things you can check with strings
-print(test_string[:5])
+# Information you can get from strings
+
 print(len(test_string))
 print(test_string.startswith('Yeti'))
 print(test_string.endswith('Yeti'))
 
 word = 'Ness'
 
+# Find the offset of the first occurence of a word
+
 print(test_string.find(word))
+
+# Find the offset of the last occurence of a word
+
 print(test_string.rfind(word))
+
+# Find the total number of occurences
+
 print(test_string.count(word))
+
+# Check if all characters are letters or numbers only (T/F)
+
 print(test_string.isalnum())
 
-# remove from the beginning and end:
+# remove characters from the beginning and end:
+
 test_string = test_string.strip('.')
 print(test_string)
 
-# replace haracters or words:
-# you can also use a number argumen to limit the number of replacements:
-# like this: test_string.replace(',', '', 2)
-test_string = test_string.replace(',', '')
+# Replace characters or words. You can also use a number argument 
+# to limit the number of replacements like this: test_string.replace(' ', '-', 2)
+
+test_string = test_string.replace(' ', '-')
 print(test_string)
 
-# change case:
+test_string = test_string.replace('unicorn', 'red bull')
+print(test_string)
+
+# Change case:
 test_string = test_string.lower()
 print(test_string)
 
@@ -65,15 +82,13 @@ print(test_string)
 test_string = test_string.swapcase()
 print(test_string)
 
-# alignemnt:
-test_string = test_string.center(60)
+# Alignemnt (not sure why you would ever do this):
+
+test_string = test_string.center(20)
 print(test_string)
 
-test_string = test_string.ljust(60)
+test_string = test_string.ljust(20)
 print(test_string)
 
-test_string = test_string.rjust(60)
-print(test_string)
-
-test_string = test_string.replace('unicorn', 'redbull')
+test_string = test_string.rjust(20)
 print(test_string)
