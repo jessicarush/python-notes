125f3531f6f1bde04e067eae9ab5aed30e8ea974
Jessica Rush <info@cyan.red>
2016-10-17 14:21:02 -0700

    Create while_loops.py

---
+++ while_loops.py
@@ -0,0 +1,24 @@
+# While loop with break, continue examples
+
+def test1():
+    while True:
+        stuff = input('Capitalize text [type q to quit]: ')
+        if stuff == 'q':
+            break
+        else:
+            print(stuff.capitalize())
+
+def test2():
+    while True:
+        value = input('Interger [q to quit]:')
+        if value == 'q':
+            break
+        try:
+            number = int(value)
+        except ValueError:
+            continue
+        number = int(value) 
+        print(number, 'squared is', number * number)
+
+test1()
+test2()
