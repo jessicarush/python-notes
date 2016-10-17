554bd59fe36dbf2cc130e4c73cc708eabaf5f6be
Jessica Rush <info@cyan.red>
2016-10-17 08:40:22 -0700

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,29 @@
+# Tuples
+
+# remember tuples are immutable: you can't add, change or delete items
+colours_tuple = ('green', 'blue', 'red')
+
+#tuples let you assign multiple variables at once:
+a, b, c = colours_tuple
+print(a)
+print(b)
+print(c)
+
+#you can use tuples to swap variable values in one line:
+a, c = c, a
+print(a)
+print(b)
+
+# the tuple() function lets you convert something to a tuple:
+colours_list = ['orange', 'yellow', 'purple']
+colours_tuple = tuple(colours_list)
+print(colours_tuple)
+
+print("""
+Benefits of using tuples:
+- Tuples use less space
+- You can't mess with tuple items by mistake
+- You can use tuples and dictionary keys
+- Named tuples can be a simple alternative to objects
+- Function arguments are passed as tuples
+""")
