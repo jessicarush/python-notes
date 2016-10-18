9ce6265574e3f4703e9799dfb2365e5fdbebce47
Jessica Rush <info@cyan.red>
2016-10-17 17:11:59 -0700

    Create error_handling.py

---
+++ error_handling.py
@@ -0,0 +1,38 @@
+# Handle errors with try and except
+
+shortlist = [1, 2, 3]
+position = 5
+
+try:
+    shortlist[position]
+except:
+    print('Need a position between 0 and', len(shortlist)-1, '. Value inserted is no good:', position)
+
+
+# Handle different types of exceptions using Pyhton standard exception names:
+
+newshortlist = [1, 2, 3]
+
+while True:
+    value = input('Position [q to quit]? ')
+    if value == 'q':
+        break
+    try:
+        position = int(value)
+        print(newshortlist[position])
+    except IndexError as err:
+        print('Bad index:', position)
+    except Exception as other:
+        print('Something else broke:', other)
+
+# Make you rown exceptions
+# an exception is a class, It's a child of the base class Exception
+
+class UppercaseException(Exception):
+    pass
+
+words = ['one', 'two', 'THREE', 'four']
+
+for word in words:
+    if word.isupper():
+        raise UppercaseException(word)
