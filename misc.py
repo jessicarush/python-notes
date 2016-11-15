0e0da7b65286e2a8b843327314e03992d835f22a
Jessica Rush <info@cyan.red>
2016-11-14 18:53:53 -0800

    Update misc.py

--- misc.py
+++ misc.py
@@ -6,8 +6,11 @@ long_text = 'A long time ago' + \
     ' in a galaxy far, far away' + \
     ' blah blah bla'
 
-# None is not the same as False, though it may look valse when evaluated as a boolean
-# None is technically none as seen here:
+# None
+"""
+None is not the same as False. Though it may look false when evaluated as a boolean,
+None is technically none as seen here:
+"""
     
 thing = None 
 
@@ -20,3 +23,20 @@ def is_none(thing):
         print("it must be False")
 
 is_none(thing)
+
+# The enumerate() function
+"""
+The enumerate function takes apart a list and feeds each item to the for loop, 
+adding a number to each item as a bonus. To compare, test a regular for loop first.
+"""
+
+for colour in colours:
+    print(colour)
+
+# If a start number isn't specified, the first item is 0
+for colour in enumerate(colours):
+    print(colour)
+
+# Start at 1 instead:
+for number, colour in enumerate(colours, 1):
+    print(number, colour)
