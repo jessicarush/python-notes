24a582ffe19359bff5b6d093c6f8052591e56360
Jessica Rush <hello@cyan.red>
2017-07-31 10:31:37 -0700

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,6 +1,15 @@
 # Python philosophy
 import this 
 
+# Functions that convert types
+float(True)                                 # returns 1.0
+int(True)                                   # returns 1
+str(True)                                   # returns True
+list((1,2,3))                               # returns [1, 2, 3]
+dict((('a', 'A'), ('b', 'B'), ('c', 'C')))  # returns {'c':'C', 'a': 'A', 'b': 'B'}
+set((('a'), ('b'), ('c')))                  # returns {'b', 'c', 'a'}
+tuple((('a'), ('b'), ('c')))                # returns ('a', 'b', 'c')
+
 # Continuing lines with \
 long_text = 'A long time ago' + \
     ' in a galaxy far, far away' + \
