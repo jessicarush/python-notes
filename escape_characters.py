156ad11a278c334746c6964844d9a9384732d747
Jessica Rush <hello@cyan.red>
2017-08-08 12:35:19 -0700

    Create escape_characters.py

---
+++ escape_characters.py
@@ -0,0 +1,24 @@
+# Escape Characters
+
+# \ allows you to escape characters or run an escape sequence
+# for example, escape the single-quote inside the string:
+
+print('I am 6\'2" tall.')    
+
+# using triple quotes also works """
+
+# \\ = backslash
+# \' = single quote
+# \" = double quote
+# \a = ASCII bell
+# \b = ASCII backspace
+# \f = ASCII formfeed
+# \n = ASCII line break
+# \r = ASCII full return
+# \t = ASCII tab
+# \v = ASCII vertical tab
+# \N{name} = unicode character name in the unicode database
+# \uxxxx = unicode character with 16-bit hex value xxxx
+# \Uxxxxxxxx = unicode character with 32-bit hex value xxxxxxxx
+# \ooo = character with octal value ooo
+# \xhh... = character wuth hex value hh..
