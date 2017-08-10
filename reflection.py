f4d3ec4808fbd11d529c66eb0914c92ceedf8ab5
Jessica Rush <hello@cyan.red>
2017-08-09 17:57:14 -0700

    Create reflection.py

---
+++ reflection.py
@@ -0,0 +1,16 @@
+type()
+
+import datetime
+
+now = datetime.datetime.utcnow()
+
+type(now)       # <class 'datetime.datetime'>
+type(234)       # <class 'int'>
+type('hello')   # <class 'str'>
+
+# isinstance()
+# Because everything in Python is an object, isinsatnce works everywhere:
+
+isinstance(now, datetime.datetime)  # True
+isinstance(234, int)                # True
+isinstance('hello', str)            # True
