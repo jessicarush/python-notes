27c3bcc7ebf77281a71b7c169ca54e49212a5e56
Jessica Rush <hello@cyan.red>
2017-08-18 11:57:21 -0700

    Create resources.py

---
+++ resources.py
@@ -0,0 +1,8 @@
+# Resources:
+
+# When looking for code:
+
+# Python standrad Library - https://docs.python.org/3/library/
+# PyPI - https://pypi.python.org/pypi
+# Github - https://github.com/trending?l=python
+# Active State - http://code.activestate.com/recipes/langs/python/
