d16a24e2b3b34637d857fe3afe44213461dd6a07
hello@cyan.red <hello@cyan.red>
2017-09-07 18:42:28 -0700

    Created new file with code examples and comments

---
+++ recursion.py
@@ -0,0 +1,80 @@
+'''Recursion'''
+
+# Recursion is a way of programming or coding a problem, in which a function
+# calls itself one or more times in its body. Usually, it is returning the
+# return value of this function call. Put simply, a recursive function is a
+# function that calls itself.
+
+# Factorial ------------------------------------------------------------------
+
+def factorial_i(n):
+    '''calculates n! iteratively'''
+    # for example  5! = 5 * 4 * 3 * 2 * 1 = 120
+    result = 1
+    if n > 1:
+        for f in range(2, n + 1):
+            result *= f
+    return result
+
+
+# With factorials, you can also say that 6! = 6 * 5!, which is to say you can
+# get the factorial of 6 by multiplying 6 * 5's factorial (120).
+
+def factorial_r(n):
+    '''calculates n! recursively'''
+    # n! can also be defined as n * (n-1)!
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial_r(n - 1)
+
+# Termination condition: A recursive function has to terminate to be useable
+# in a program. A recursive function terminates, if with every recursive call
+# the solution of the problem is downsized and moves towards a base case.
+# A base case is, where the problem can be solved without further recursion.
+# A recursion can lead to an infinite loop, if the base case is not met. In the
+# example above, the base case it met by if n <= 1, return 1
+
+# testing:
+for i in range(20):
+    print(i, factorial_i(i))
+
+for i in range(20):
+    print(i, factorial_r(i))
+
+# Fibonacci ------------------------------------------------------------------
+
+def fib_r(n):
+    '''Calculates fibonacci recursively F(n) = F(n - 1) + F(n -2)'''
+    if n  < 2:
+        return n
+    else:
+        return fib_r(n - 1) + fib_r(n - 2)
+
+def fib_i(n):
+    '''Calculates fibonacci iteratively'''
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+def fib_g(n):
+    '''Calculates fibonacci with a generator'''
+    curr = 1
+    prev = 0
+    counter = 0
+    while counter < n:
+        yield curr
+        prev, curr = curr, prev + curr
+        counter += 1
+
+# testing:
+for i in range(20):
+    print(fib_r(i))
+
+for i in range(20):
+    print(fib_i(i))
+
+fib_generator = fib_g(20)
+for i in fib_generator:
+    print(i)
