28abb00ec80b4db2c3f0e1c6df4e58ea065b0a29
hello@cyan.red <hello@cyan.red>
2017-11-06 16:00:07 -0800

    Initial commit

---
+++ pygal_api_example1.py
@@ -0,0 +1,138 @@
+'''Data visualization using a web API'''
+
+# A program that downloads current information about the most-starred
+# Python projects in GitHub:
+# https://api.github.com
+# http://docs.python-requests.org
+
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+# -----------------------------------------------------------------------------
+# Processing an API response
+# -----------------------------------------------------------------------------
+
+# make an API call and store the requests:
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+# It's always go to check the status - use these to make some asserts.
+print('Status code:', r.status_code)
+# -> Status code: 200  (a status code of 200 indicates a successful response)
+print('Headers:', r.headers['content-type'])
+# -> Headers: application/json; charset=utf-8
+print('Encoding:', r.encoding)
+# -> Encoding: utf-8
+
+# Store API response in a variable. The API returns info in JSON format so we
+# use the json() method to convert to a python dict.
+response_dict = r.json()
+print(type(response_dict))  # -> <class 'dict'>
+
+# process results:
+print(response_dict.keys())
+# -> dict_keys(['total_count', 'incomplete_results', 'items'])
+print('Total repositories:', response_dict['total_count'])
+# -> Total repositories: 2077471
+repo_dicts = response_dict['items']
+print('Repositories returned:', len(repo_dicts))
+# -> Repositories returned: 30
+
+# examine the first repo:
+repo_dict = repo_dicts[0]
+print('\nKeys:', len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
+
+# -----------------------------------------------------------------------------
+# Summarizing the top repos
+# -----------------------------------------------------------------------------
+
+print('\nSome information about the top 30 repository:')
+for repo_dict in repo_dicts:
+    print('\nName:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    print('Updated:', repo_dict['updated_at'])
+    print('Description:', repo_dict['description'])
+
+# -----------------------------------------------------------------------------
+# Monitoring API rate limits
+# -----------------------------------------------------------------------------
+
+# most APIs are rate-limited, which means there's a limit to how many requests
+# you can make in a certain amount of time. To see if you're approaching
+# GitHub's limits: https://api.github.com/rate_limit
+
+# -----------------------------------------------------------------------------
+# Visualizing the repos with pygal
+# -----------------------------------------------------------------------------
+
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+my_style = LS('#25bec4', base_style=LCS)
+my_style.title_font_family = 'Helvetica'
+
+# NOTE: google fonts will only render when the svg is embedded because the
+# google stylesheet is added in the XML processing.
+# my_style.title_font_family = 'googlefont:Slabo'
+
+my_style.title_font_size = 14
+my_style.foreground = '#586e75'
+my_style.foreground_strong = '#334145'
+my_style.foreground_subtle = '#fdca32'
+my_style.label_font_size = 9
+my_style.major_label_font_size = 12
+my_style.tooltip_font_size = 11
+
+my_config = pygal.Config()
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = True
+my_config.width = 1000
+
+my_config.y_labels = list(range(0, 42000, 2000))
+my_config.y_labels_major_count = 3
+my_config.x_labels = names
+my_config.x_label_rotation = 45
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = "Most-Starred Python Projects on GitHub"
+
+# NOTE: You can add custom tooltips by passing a list of dictionaries instead
+# of a list of values to add() like so:
+# plot_dict = [
+#     {'value': 1314, 'label': 'Description of ...'},
+#     {'value': 5670, 'label': 'Description of ...'},
+#     {'value': 3224, 'label': 'Description of ...'},
+# ]
+
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    # get the description if one is available:
+    description = repo_dict['description']
+    if not description:
+        description = 'No description provided'
+    if len(description) > 118:
+        description = description[:118] + '...'
+
+# NOTE: pygal uses the value given for 'xlink' to turn each bar into an active
+# link to the given url. You can also add links to the legend. See the docs:
+# http://pygal.org/en/stable/documentation/configuration/value.html#legend
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': description,
+        'xlink' : repo_dict['html_url']
+        }
+    plot_dicts.append(plot_dict)
+
+# chart.add('', stars)
+chart.add('', plot_dicts)
+chart.render_to_file('api_pygal_example.svg')
---
+++ pygal_api_example2.py
@@ -0,0 +1,189 @@
+'''Exploring API Calls'''
+
+# The following call returns information about an article on hacker news:
+# https://hacker-news.firebaseio.com/v0/item/9884165.json
+
+# The dictionary contains of number of useful keys like title, url, score.
+# The 'descendants' key contains the number of comments an article has.
+
+import json
+from operator import itemgetter
+
+import requests
+import pygal
+from pygal.style import DefaultStyle as DS, LightenStyle as LS
+
+# -----------------------------------------------------------------------------
+# Make an API call and store the response:
+# -----------------------------------------------------------------------------
+
+# This API returns a list containing the IDs of top 500 articles on HN:
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+
+print('Status code:', r.status_code)
+print('Headers:', r.headers['content-type'])
+print('Encoding:', r.encoding)
+
+# -----------------------------------------------------------------------------
+# Process information about each submission:
+# -----------------------------------------------------------------------------
+
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:100]:
+    # make a separate API call for each submission:
+    url = ('https://hacker-news.firebaseio.com/v0/item/'
+            + str(submission_id) + '.json')
+    submission_r = requests.get(url)
+    response_dict = submission_r.json()
+
+    submission_dict = {
+        'title' : response_dict['title'],
+        'link': 'http://news.ycombinator.com/item?id=' + str(submission_id),
+        'comments': response_dict.get('descendants', 0),
+        'id' : str(submission_id)
+        }
+    submission_dicts.append(submission_dict)
+
+# -----------------------------------------------------------------------------
+# Store the dict as a JSON to reuse while testing:
+# -----------------------------------------------------------------------------
+
+with open('hacker_news_api.json', 'w') as fob:
+    json.dump(submission_dicts, fob)
+
+with open('hacker_news_api.json') as fob:
+    submission_dicts = json.load(fob)
+
+# -----------------------------------------------------------------------------
+# Sort and visualize the information:
+# -----------------------------------------------------------------------------
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
+                            reverse=True)
+
+submission_dicts = list(submission_dicts[15:0:-1])
+
+titles, plot_dicts = [], []
+for submission_dict in submission_dicts:
+    title = submission_dict['title']
+    if len(title) > 100:
+        title = title[:100] + '...'
+    titles.append(title)
+
+    plot_dict = {
+        'value': submission_dict['comments'],
+        'label': submission_dict['id'],
+        'xlink' : submission_dict['link'],
+        'title' : title
+        }
+    plot_dicts.append(plot_dict)
+
+
+# -----------------------------------------------------------------------------
+# Style info:
+# -----------------------------------------------------------------------------
+
+style = LS('#25bec4', base_style=DS)
+
+# style.plot_background = '#586e75'
+# style.background = '#586e75'
+style.foreground = '#586e75'             # all labels, guides
+style.foreground_strong = '#13434f'      # major labels and title
+# style.foreground_subtle = '#fdca32'    # minor guides
+
+# style.font_family = 'Helvetica'
+style.title_font_family = 'Helvetica'
+style.label_font_family = 'Helvetica'
+# style.major_label_font_family = 'Helvetica'
+# style.value_font_family = 'Helvetica'
+# style.value_label_font_family = 'Helvetica'
+# style.tooltip_font_family = 'Helvetica'
+# style.legend_font_family = 'Helvetica'
+# style.no_data_font_family = 'Helvetica'
+
+style.title_font_size = 18
+style.label_font_size = 13
+style.major_label_font_size = 10
+# style.value_font_size = 8
+# style.value_label_font_size = 8
+# style.tooltip_font_size = 11
+# style.legend_font_size = 10
+# style.no_data_font_size = 48
+
+# style.guide_stroke_dasharray = '2, 4'
+style.major_guide_stroke_dasharray = '2, 4'
+style.opacity = '.6'
+style.opacity_hover = '.9'
+# style.transition = '150ms'
+# style.colors = ('#E853A0', '#E8537A', '#E95355', '#E87653', '#E89B53')
+# style.value_colors = ()
+
+# -----------------------------------------------------------------------------
+# Config info:
+# -----------------------------------------------------------------------------
+
+config = pygal.Config()
+config.width = 1200
+# config.height = 500
+# config.spacing = 50
+# config.x_title = 'x-axis'
+# config.y_title = 'y-axis'
+# config.human_readable = True   # for complex or large numbers
+# config.stroke = False          # for line graphs
+# config.fill = True             # for line graphs
+# config.show_dots = False       # for line graphs
+# config.dots_size = 5           # for line graphs
+# config.rounded_bars = 20       # for bar graphs
+# config.half_pie = True         # for pie charts
+# config.inner_radius = .6       # for pie charts
+
+# config.margin = 0
+# config.margin_top = 10
+config.margin_right = 100
+# config.margin_bottom = 10
+config.margin_left = -40
+
+config.show_legend = False
+# config.truncate_legend = -1
+# config.legend_box_size = 15
+# config.legend_at_bottom = True
+# config.legend_at_bottom_columns = 4
+
+config.show_x_labels = True
+config.x_labels = titles
+# config.x_label_rotation = 45
+# config.x_labels_major = [0, 5, 10]
+# config.x_labels_major_every = 3
+# config.x_labels_major_count = 5
+# config.show_minor_x_labels = True
+
+config.show_y_labels = True
+# config.y_labels = names
+# config.y_label_rotation = 45
+# config.y_labels_major = [0, 50, 100]
+config.y_labels_major_every = 1
+# config.y_labels_major_count = 2
+config.show_minor_y_labels = True
+
+# config.truncate_label = 15
+
+# config.inverse_y_axis = True
+# config.inverse_x_axis = True
+# config.print_values = True
+# config.dynamic_print_values = True
+# config.tooltip_border_radius = 3
+# my_config.show_y_guides = True
+# my_config.show_x_guides = True
+
+# -----------------------------------------------------------------------------
+# Create and populate the plot:
+# -----------------------------------------------------------------------------
+
+chart = pygal.HorizontalBar(config, style=style)
+chart.title = "Most-commented articles on Hacker News top 100"
+
+chart.add('', plot_dicts)
+
+chart.render_to_file('hacker_news_api.svg')
