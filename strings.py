1f94eb229ba946572977b5d3e974991a4d2399bb
hello@cyan.red <hello@cyan.red>
2017-11-15 14:11:15 -0800

    Improved code examples

--- strings.py
+++ strings.py
@@ -20,55 +20,59 @@ dir(str)
 my_string = 'Some content'
 my_string = "Some content"
 
-# Concatenate strings with +, if the lines are long add \:
+# Concatenate strings with '+'. If the lines are long you can add '\':
 
-long_string = 'A long time ago ' + 'in a galaxy far, far away ' + \
-    'blah blah bla...'
-
-print(long_string)
+long_string1 = 'A long time ago, ' + 'in a galaxy far, far away... ' + \
+              'blah blah bla...'
 
 # That being said, many feel the use of \ should be minimized. When possible,
-# use parenthesis and concatenation instead:
+# use parenthesis instead. When strings fall on new lines, you don't need '+':
 
-long_string = ('A long time ago '
-               'in a galaxy far, far away '
-               'blah blah blabity...')
+long_string2 = ('A long time ago, ' + 'in a galaxy far, far away... '
+              'blah blah bla...')
+
+long_string3 = ('A long time ago, '
+               'in a galaxy far, far away... '
+               'blah blah bla...')
+
+print(long_string1)
+print(long_string2)
+print(long_string3)
+# A long time ago, in a galaxy far, far away... blah blah bla...
+# A long time ago, in a galaxy far, far away... blah blah bla...
+# A long time ago, in a galaxy far, far away... blah blah bla...
+
+# You can also use triple quotes for long strings, but I think these are
+# best reserved for docstrings (see documenting_naming.py). When using triple
+# quotes, all invisible line breaks and spaces are included in the output.
+
+long_string = """A long time ago,
+in a galaxy far, far away...
+It is a period of civil war."""
 
 print(long_string)
-
-# You can also use triple quotes for long strings, but I think these are best
-# reserved for docstrings (see documenting_naming.py):
-
-long_string = """This string uses
-triple quotes because we're pretending
-it's very, very long"""
-print(long_string)
-
-# Each line in the above will appear on a new line. If you don't want that,
-# escape the last invisible line break character with \
-
-long_string = """This string uses \
-triple quotes because we're pretending \
-it's very, very long"""
-print(long_string)
+# A long time ago,
+# in a galaxy far, far away...
+# It is a period of civil war.
 
 # -----------------------------------------------------------------------------
-# str() - to convert to a string
+# Convert to a string with .str()
 # -----------------------------------------------------------------------------
 
+text = 'The number is '
 number = 4.5 * 3.25
-print(str(number))
+print(text + str(number))  # The number is 14.625
 
 # -----------------------------------------------------------------------------
 # Slice with [start : end : step]
 # -----------------------------------------------------------------------------
 
 letters = 'abcdefghijk'
-print(letters[:-1])
-print(letters[2:])
-print(letters[2:8])
-print(letters[:8])
-print(letters[2:8:2])
+print(letters[:3])     # abc
+print(letters[-3:])    # ijk
+print(letters[:6:2])   # ace
+print(letters[1:8:3])  # beh
+print(letters[::-1])   # kjihgfedcba
 
 # -----------------------------------------------------------------------------
 # Break a string up into a list with .split()
@@ -99,104 +103,106 @@ print(type(splitline_list))  # <class 'list'>
 # -----------------------------------------------------------------------------
 
 test_string = ' '.join(test_list)
-print(test_string)
-print(type(test_string))
+print(test_string)        # Yeti Bigfoot Loch Ness Unicorn...
+print(type(test_string))  # <class 'str'>
 
 # -----------------------------------------------------------------------------
-# Information you can get from strings
+# .len(), .startswith(), .endswith(), .find(), .rfind(), .count(), .isalnum()
 # -----------------------------------------------------------------------------
 
-print(len(test_string))
-print(test_string.startswith('Yeti'))
-print(test_string.endswith('Yeti'))
-print('Bigfoot' in test_string)
-
-word = 'Ness'
+print(len(test_string))                # 33
+print(test_string.startswith('Yeti'))  # True
+print(test_string.endswith('Yeti'))    # False
+print('Bigfoot' in test_string)        # True
 
 # Find the offset of the first occurrence of a word
 
-print(test_string.find(word))
+word = 'Ness'
+
+print(test_string.find(word))  # 18
 
 # Find the offset of the last occurrence of a word
 
-print(test_string.rfind(word))
+print(test_string.rfind(word))  # 18
 
 # Find the total number of occurrences
 
-print(test_string.count(word))
+print(test_string.count(word))  # 1
 
 # Check if all characters are letters or numbers only (T/F)
 
-print(test_string.isalnum())
+print(test_string.isalnum())  # False
 
 # -----------------------------------------------------------------------------
-# Remove and Replace
+# Remove and replace: .lstrip(), .rstrip(), .strip(), .replace()
 # -----------------------------------------------------------------------------
-# remove characters from the beginning lstrip(), end rstrip() or both strip():
-name = ' .Raja. '
-name = name.lstrip()    #--> '.Raja. '
-name = name.rstrip()    #--> '.Raja.'
-name = name.strip('.')  #--> 'Raja'
-print(name)
+# remove characters from the beginning with .lstrip(), the end with .rstrip()
+# or both with .strip():
+
+name = '- Raja -'
+name = name.lstrip('-')  # ' Raja -'
+name = name.rstrip('-')  # ' Raja '
+name = name.strip(' ')   # 'Raja'
 
 # Replace characters or words. You can also use a number argument
 # to limit the number of replacements like: test_string.replace(' ', '-', 2)
 
 name = 'Yello'
-print(name.replace('Y', 'H'))
+print(name.replace('Y', 'H'))  # Hello
 
-test_string = test_string.replace(' ', '-')
-print(test_string)
+test_string = test_string.replace(' ', ', ')
+print(test_string)  # Yeti, Bigfoot, Loch, Ness, Unicorn...
 
-test_string = test_string.replace('unicorn', 'red bull')
-print(test_string)
+test_string = test_string.replace('Unicorn', 'Dragon')
+print(test_string)  # Yeti, Bigfoot, Loch, Ness, Dragon...
 
 # -----------------------------------------------------------------------------
 # Change Case
 # -----------------------------------------------------------------------------
 
 test_string = test_string.lower()
-print(test_string)
+print(test_string)  # yeti, bigfoot, loch, ness, dragon...
 
 test_string = test_string.upper()
-print(test_string)
+print(test_string)  # YETI, BIGFOOT, LOCH, NESS, DRAGON...
 
 test_string = test_string.capitalize()
-print(test_string)
+print(test_string)  # Yeti, bigfoot, loch, ness, dragon...
 
 test_string = test_string.title()
-print(test_string)
+print(test_string)  # Yeti, Bigfoot, Loch, Ness, Dragon...
 
 test_string = test_string.swapcase()
-print(test_string)
+print(test_string)  # yETI, bIGFOOT, lOCH, nESS, dRAGON...
 
 # NOTE: .lower() and .upper() are particularly useful when you're iterating
 # over a string looking for something but you don't care about the case or,
 # you receive input and you don't want to handle upper and lower variations:
 
 if 'bigfoot' in test_string.lower():
-    print("He's there")
+    print("He's there")  # He's there
 
 # NOTE: title() doesn't handle apostrophes very well. Use capwords() instead:
 
-example = "I'm super fun."
-print(example.title())
-
 from string import capwords
-print(capwords(example))
+
+example = "I'm super fun."
+
+print(example.title())    # I'M Super Fun.
+print(capwords(example))  # I'm Super Fun.
 
 # -----------------------------------------------------------------------------
 # Alignment
 # -----------------------------------------------------------------------------
 
-test_string = test_string.center(20)
-print(test_string)
+test1 = example.center(20)
+test2 = example.ljust(20)
+test3 = example.rjust(20)
 
-test_string = test_string.ljust(20)
-print(test_string)
+test_list = [test1, test2, test3]
 
-test_string = test_string.rjust(20)
-print(test_string)
+print(test_list)
+# ["   I'm super fun.   ", "I'm super fun.      ", "      I'm super fun."]
 
 # -----------------------------------------------------------------------------
 # Raw string type literals
