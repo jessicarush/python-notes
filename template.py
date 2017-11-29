4fded3501d44a9100991404ce8900eec73bf3902
hello@cyan.red <hello@cyan.red>
2017-11-28 18:28:44 -0800

    Initial commit

---
+++ template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+'''Docstring Summary Line, followed by a space.
+
+Followed by the extended description of the module. This may include
+special notes on structire and usage as well as include brief information
+on the modules Classes, Functions and variables.
+'''
+
+import datetime as dt
+from random import choice
+
+
+__author__ =     "Jessica Rush"
+__copyright__ =  "Copyright 2017, Cyan Red"
+__credits__ =    ["Scott Volk", "Someone Else"]
+__license__ =    "GPL"
+__version__ =    "1.0.1"
+__maintainer__ = "Jessica Rush"
+__email__ =      "jesskrush@me.com"
+__status__ =     "Production"
+
+
+# Explanation: The above is a recommended example source code file header.
+# The main things to note here:
+
+# 1. The first line of each file should be #!/usr/bin/env python3 (or the path #    to wherever your installation is... see which python3 for your path).
+#    This makes it possible to run the file as a script invoking the
+#    interpreter implicitly, (basically, just type $ templates.py in the
+#    command line instead of $ python3 templates.py).
+
+# 2. Next should be the docstring with a description. If the description is
+#    long, the first line should be a short summary that makes sense on its
+#    own, separated from the rest by a newline.
+
+# 3. Third should be all the import statements. Standard library first, then
+#    third-party libraraies, then your own modules.
+
+# 4. OPTIONAL: Authorship information. Note that many feel this information
+#    should be kept out of the source file and kept in its own file(s) such
+#    as AUTHORS and LISCENSE. Status should typically be one of "Prototype",
+#    "Development", or "Production". More information can be found here:
+#    http://epydoc.sourceforge.net/manual-fields.html#module-metadata-variables
