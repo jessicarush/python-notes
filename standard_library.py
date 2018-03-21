3fa1e96fde719b2e1a0e99872411c8f98529a298
hello@cyan.red <hello@cyan.red>
2018-03-20 19:20:34 -0700

    Added itemgetter() examples

--- standard_library.py
+++ standard_library.py
@@ -2,7 +2,8 @@
 
 # https://docs.python.org/3/library/index.html
 
-# -----------------------------------------------------------------------------
+from pprint import pprint
+
 # Counter()
 # -----------------------------------------------------------------------------
 
@@ -14,8 +15,8 @@ jb_counter = Counter(jellybeans)
 print(jb_counter)
 # Counter({'red': 3, 'green': 2, 'orange': 1})
 
-# most_common() returns all elements in descending order
-# (or just the top count if optional value passed in)
+# most_common() returns all elements in descending order or just the top
+# count if optional value passed in:
 
 print(jb_counter.most_common(1))
 # [('red', 3)]
@@ -39,7 +40,7 @@ print(jb_counter2 - jb_counter1)
 print(jb_counter1 & jb_counter2)
 # Counter({'red': 1})
 
-# -----------------------------------------------------------------------------
+
 # deque()
 # -----------------------------------------------------------------------------
 # is a double-ended queue which has features of both a stack and a queue.
@@ -66,9 +67,9 @@ palindrome('what')      # returns False
 def palindrome_better(word):
     return word == word[::-1]
 
-palindrome_better('radar')    # returns True
+palindrome_better('radar')  # returns True
 
-# -----------------------------------------------------------------------------
+
 # itertools
 # -----------------------------------------------------------------------------
 # itertools are special purpose iterator functions.
@@ -80,7 +81,8 @@ import itertools
 # chain() – runs through its arguments as if they were a single iterable
 
 for item in itertools.chain([1, 2], ['a', 'b']):
-    print(item) # 1, 2, a, b
+    print(item)
+    # 1, 2, a, b
 
 # cycle() – is an infinite iterator, cycling through its arguments forever:
 
@@ -90,7 +92,8 @@ for item in itertools.chain([1, 2], ['a', 'b']):
 # accumulate() – calculates accumulated values. By default, the sum:
 
 for item in itertools.accumulate([1, 2, 3, 4]):
-    print(item)  # 1, 3, 6, 10
+    print(item)
+    # 1, 3, 6, 10
 
 # you can provide a function as a second argument to accumulate().
 # this will be used instead of addition. The function should take two
@@ -100,9 +103,10 @@ def multiply(a, b):
     return a * b
 
 for item in itertools.accumulate([1, 2, 3, 4], multiply):
-    print(item)  # 1, 2, 6, 24
+    print(item)
+    # 1, 2, 6, 24
 
-# -----------------------------------------------------------------------------
+
 # os.walk()
 # -----------------------------------------------------------------------------
 # os.walk recursively visits each directory from the root, and for each one,
@@ -145,7 +149,7 @@ for path, directories, files in os.walk(root, topdown=True):
 # Using this you could easily create a database or structured file format,
 # pulling out the specific bits of information.
 
-# -----------------------------------------------------------------------------
+
 # difflib
 # -----------------------------------------------------------------------------
 # the difflib module contains tools for comparing and working with differences
@@ -155,14 +159,18 @@ for path, directories, files in os.walk(root, topdown=True):
 from difflib import SequenceMatcher
 
 test = SequenceMatcher(None, 'rain', 'rainn')
-print(type(test))    # <class 'difflib.SequenceMatcher'>
-print(test.ratio())  # 0.8888888888888888
+print(type(test))
+# <class 'difflib.SequenceMatcher'>
+print(test.ratio())
+# 0.8888888888888888
 
 test = SequenceMatcher(None, 'rain', 'Rainn')
-print(test.ratio())  # 0.6666666666666666
+print(test.ratio())
+# 0.6666666666666666
 
 test = SequenceMatcher(None, ' rain', 'r a i n n ')
-print(test.ratio())  # 5333333333333333
+print(test.ratio())
+# 5333333333333333
 
 # The ratio method returns the similarity between two strings on a scale of 0–1.
 # Note, it is case sensitive. Extra characters such as spaces and '\n' also
@@ -182,5 +190,34 @@ from difflib import get_close_matches
 #      Possibilities that don't score at least that similar to word are ignored
 
 possibilities = ['train', 'car', 'grain', 'arithmetic', 'rain', 'ball']
-print(get_close_matches('rainn', possibilities))  # ['rain', 'train', 'grain']
-print(get_close_matches('rainn', possibilities, n=1))  # ['rain']
+
+print(get_close_matches('rainn', possibilities))
+# ['rain', 'train', 'grain']
+print(get_close_matches('rainn', possibilities, n=1))
+# ['rain']
+
+
+# itemgetter()
+# -----------------------------------------------------------------------------
+# This method allows us to sort lists of dictionaries or tuples by something
+# other than their first value. For example:
+
+from operator import itemgetter
+
+l = [('bob', 50), ('mary', 45), ('rick', 72), ('jane', 28)]
+
+l.sort(key=itemgetter(1))
+
+print(l)
+# [('jane', 28), ('mary', 45), ('bob', 50), ('rick', 72)]
+
+l = [{'id': 6, 'username': 'bob', 'email': 'bob@email.com'},
+     {'id': 1, 'username': 'zed', 'email': 'zed@email.com'},
+     {'id': 4, 'username': 'jane', 'email': 'jane@email.com'}]
+
+l.sort(key=itemgetter('username'), reverse=True)
+
+pprint(l)
+# [{'email': 'zed@email.com', 'id': 1, 'username': 'zed'},
+#  {'email': 'jane@email.com', 'id': 4, 'username': 'jane'},
+#  {'email': 'bob@email.com', 'id': 6, 'username': 'bob'}]
