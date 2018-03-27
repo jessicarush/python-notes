3d70b7b37e7203a481de1cf81e95d09d6b3476f6
hello@cyan.red <hello@cyan.red>
2018-03-27 11:41:25 -0700

    Corrected varibale name my_logger as logger

--- logging_errors.py
+++ logging_errors.py
@@ -19,14 +19,15 @@ fmt = '%(asctime)s %(levelname)s %(lineno)s %(message)s'
 logging.basicConfig(level=logging.DEBUG, filename='test.log', format=fmt)
 
 # priority levels:
-
 logging.debug('debug message')
 logging.info('info message')
 logging.warn('warning message')
 logging.error('error message')
 logging.critical('critical message')
 
-# T he default priority level is warn. Set the default by using basicConfig().
+# Breakdown
+# -----------------------------------------------------------------------------
+# The default priority level is warn. Set the default by using basicConfig().
 # This config should appear before any other logging functions as above.
 
 logging.basicConfig(level=logging.DEBUG)
@@ -42,13 +43,13 @@ logging.basicConfig(level=logging.DEBUG, filename='test.log', format=fmt)
 
 # here's a logger object:
 
-my_logger = logging.getLogger('MyLogger')
-logger.debug('debug message')
+logger = logging.getLogger('MyLogger')
+logger.debug('here is my debug message')
 
 # Calling basicConfig() with a filename argument created a FileHandler and made
 # it available to the logger. The logging module includes at least 15 handlers
 # to send messages to places such as the screen, email, web servers and files.
 
-# The information here is pretty scant. On the next review of this topic, go
+# The information here is pretty scant. For more information on this topic, go
 # through the following in Doug Hellman's Python 3 Standard Library by example:
 # p.980–986, p.563–564, p.644–647, p.650–653, p.671–674
