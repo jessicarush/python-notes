2448bc74cf78687c5c0ef9dc17255435042f3952
hello@cyan.red <hello@cyan.red>
2018-03-28 12:29:27 -0700

    Uncommented some sample code for better readabilty.

--- timezones.py
+++ timezones.py
@@ -23,33 +23,33 @@ import datetime
 # FYI pytz pulls timezone information from this database:
 # https://www.iana.org/time-zones
 
-# see all timezone:
+# see all timezones:
 
-# for x in pytz.all_timezones:
-#     print(x)
-#
-# # see all country codes:
-#
-# for x in sorted(pytz.country_names):
-#     print(x, ':', pytz.country_names[x])
-#
-# # see all country names:
-#
-# for x in sorted(pytz.country_names):
-#     print("{}: {}: {}".format(
-#           x, pytz.country_names[x], pytz.country_timezones.get(x)))
-#
-# # see names, zones and their times:
-#
-# for x in sorted(pytz.country_names):
-#     print("{}: {}".format(x, pytz.country_names[x]))
-#     if x in pytz.country_timezones:
-#         for zone in sorted(pytz.country_timezones[x]):
-#             tz_to_display = pytz.timezone(zone)
-#             local_time = datetime.datetime.now(tz=tz_to_display)
-#             print("\t{}: {}:".format(zone, local_time))
-#     else:
-#         print("\tNo timezone defined")
+for x in pytz.all_timezones:
+    print(x)
+
+# see all country codes:
+
+for x in sorted(pytz.country_names):
+    print(x, ':', pytz.country_names[x])
+
+# see all country names:
+
+for x in sorted(pytz.country_names):
+    print("{}: {}: {}".format(
+          x, pytz.country_names[x], pytz.country_timezones.get(x)))
+
+# see names, zones and their times:
+
+for x in sorted(pytz.country_names):
+    print("{}: {}".format(x, pytz.country_names[x]))
+    if x in pytz.country_timezones:
+        for zone in sorted(pytz.country_timezones[x]):
+            tz_to_display = pytz.timezone(zone)
+            local_time = datetime.datetime.now(tz=tz_to_display)
+            print("\t{}: {}:".format(zone, local_time))
+    else:
+        print("\tNo timezone defined")
 
 
 # pytz example:
