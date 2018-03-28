ee567de24f3729895a008d2e0c55566cc83b7108
hello@cyan.red <hello@cyan.red>
2018-03-28 15:48:48 -0700

    Added another example

--- context_managers.py
+++ context_managers.py
@@ -5,11 +5,11 @@
 # have two related operations you'd like to execute as a pair, with a block of
 # code in between.
 
-print('set things up') # initialization code
+print('set things up')  # initialization code
 try:
-    print('do something') # another block of code
+    print('do something')  # another block of code
 finally:
-    print('tear things down') # finalization code
+    print('tear things down')  # finalization code
 
 # Here, “set things up” could be opening a file, or acquiring some external
 # resource, and “tear things down” would then be closing the file, or releasing
@@ -21,7 +21,7 @@ finally:
 # OS – there are a maximum number of files a process can have open at one time.
 # Using a context manager ensures these resources are released.
 
-with open('filename.txt', 'w') as fileobject:
+with open('data/filename.txt', 'w') as fileobject:
     print('do something')
 
 # So the general translation is that there is a try and finally block running
@@ -47,9 +47,37 @@ with controlled_execution() as thing:
 # include saving and restoring various kinds of global state, locking and
 # unlocking resources, closing opened files, etc.
 
-# As above, you can implement a context manager as a class. At the very least a
-# context manager has __enter__ and __exit__ methods defined. Here's an example
-# of a file opening context manager:
+# As above, you can implement a context manager as a class. At the very least
+# a context manager has __enter__ and __exit__ methods defined.
+
+# Note the 3 arguments in __exit__(). In a normal situation, these are all
+# given a value of None. However, if an exception occurs inside the with block,
+# they will be set to values related to the type, value, and traceback for the
+# exception. This allows the __exit__ method to do any cleanup code that may
+# be required, even if an exception occurred.
+
+# Here's another example of a context manager that will take a sequence
+# (of random ascii characters in this case) and convert it to string.
+
+import random
+import string
+
+class StringJoiner(list):
+    def __enter__(self):
+        return self
+    def __exit__(self, type, value, tb):
+        self.result = ''.join(self)
+
+with StringJoiner() as j:
+    for i in range(15):
+        j.append(random.choice(string.ascii_letters + string.digits))
+
+print(j)
+# ['T', 'h', '9', 'M', 'F', 'v', 'y', 'Q', 'A', 'b', '6', 'n', 'r', 'x', 'i']
+print(j.result)
+# Th9MFvyQAb6nrxi
+
+# Here's an example of a file opening context manager:
 
 class File():
 
@@ -67,7 +95,7 @@ class File():
 # Just by defining __enter__ and __exit__ methods we can use it in a with
 # statement:
 
-with File('test.txt', 'w') as opened_file:
+with File('data/test.txt', 'w') as opened_file:
     opened_file.write('Hola!')
 
 # You can also construct your own context managers using the contextmanager
@@ -106,7 +134,7 @@ def working_directory(path):
     finally:
         os.chdir(current_dir)
 
-with working_directory("data/stuff"):
+with working_directory("data"):
     pass # do something within data/stuff
 
 # Then here you are back again in the original working directory
@@ -118,7 +146,7 @@ with working_directory("data/stuff"):
 # the context:
 
 from tempfile import mkdtemp
-from shutil import rmtree
+import shutil
 
 @contextmanager
 def temporary_dir(*args, **kwargs):
