48fbfc77efafd3c61a23c43a1a5ba9f3b172291b
hello@cyan.red <hello@cyan.red>
2018-05-11 09:33:50 -0700

    Renamed file: web_scraping.py, web_APIs.md

--- web_scraping.py
+++ web_scraping.py
@@ -1,4 +1,4 @@
-'''Web Services Automation'''
+'''Web Automation: Scraping'''
 
 
 # The webbrowser Module
@@ -15,41 +15,6 @@ webbrowser.open_new(url)
 # to open in a new tab (some browsers do this already via their own prefs):
 
 webbrowser.open_new_tab(url)
-
-
-# Web APIs and Representational State Transfer (REST)
-# -----------------------------------------------------------------------------
-# Instead of publishing web pages, you can provide data through a web
-# application programming interface (API). Clients access your service by
-# making requests to URLs and getting back responses containing status and
-# data. Instead of HTML pages, the data is in formats that are easier for
-# programs to consume, such as JSON or XML.
-
-# Many products claim to have a REST or RESTful interface. In practice, this
-# often only means that they have a web interface (definitions of URLs to
-# access a web service).
-
-# a RESTful service uses the HTTP verbs in specific ways:
-
-# HEAD - gets information about the resource, but not its data.
-
-# GET - retrieves the resource's data from the server. This is the standard
-#       method used by a browser. Any time you see a URL with a question mark
-#       (?) followed by a bunch of arguments, that's a GET request. GET should
-#       not be used to create, change, or delete data.
-
-# POST - updates data on the server, often used by HTML forms and web APIs.
-
-# PUT - creates a new resource
-
-# DELETE - deletes
-
-# A RESTful client can also request one or more content types from the server
-# by using HTTP request headers. For example, a complex service with a REST
-# interface might prefer its input and output to be JSON strings.
-
-# JSON is especially well suited to web client-server data interchange.
-# It's especially popular in web-based APIs, such as OpenStack.
 
 
 # Crawl and Scrape
