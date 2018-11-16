a03c2ae879aeff1da5467e949b71700188439e48
hello@cyan.red <hello@cyan.red>
2018-11-16 15:11:24 -0800

    Added notes re: changes with GoogleV3

--- geopy_module.py
+++ geopy_module.py
@@ -1,24 +1,34 @@
 '''A Brief look at Geopy'''
 
 
-# https://geopy.readthedocs.io/en/1.10.0/
+# https://geopy.readthedocs.io/en/stable/
 
 # geopy is a Python client for several popular geocoding web services. geopy
 # makes it easy to locate the coordinates of addresses, cities, countries, and
 # landmarks across the globe using third-party geocoders and other data sources.
 
-# geopy includes geocoder classes for the OpenStreetMap Nominatim, ESRI ArcGIS,
-# Google Geocoding API (V3), Baidu Maps, Bing Maps API, Yahoo! PlaceFinder,
-# Yandex, IGN France, GeoNames, NaviData, OpenMapQuest, What3Words, OpenCage,
-# SmartyStreets, geocoder.us, and GeocodeFarm geocoder services. The various
+# geopy includes geocoder classes for: ArcGIS, AzureMaps, Baidu, BANFrance,
+# Bing, DataBC, GeocodeEarth, GeocodeFarm, Geolake, GeoNames, GoogleV3, HERE,
+# IGNFrance, MapBox, OpenCage, OpenMapQuest, Nominatim, Pelias, Photon,
+# PickPoint, LiveAddress, TomTom, What3Words, Yandex. The majority of these
+# require a (not free) API key or usernamename/password paramter. The various
 # geocoder classes are located in geopy.geocoders.
+
+# As of July 2018 Google requires each request to have a paid API key.
+# https://developers.google.com/maps/documentation/geocoding/usage-and-billing
 
 # $ pip3 install geopy
 
-from geopy.geocoders import GoogleV3
+# from geopy.geocoders import GoogleV3
+# from geopy.geocoders import Yandex
+# from geopy.geocoders import DataBC
+from geopy.geocoders import Photon
 
+# geolocator = GoogleV3(scheme='http')
+# geolocator = Yandex(lang='en', scheme='http')
+# geolocator = DataBC(scheme='http')
+geolocator = Photon(scheme='http')
 
-geolocator = GoogleV3(scheme='http')
 
 address1 = '3995 23rd St, San Francisco, CA 94114, USA'
 address2 = '3730 Cambie St, Vancouver, BC V5Z2X5, Canada'
@@ -33,10 +43,12 @@ print(location)
 # 3995 23rd St, San Francisco, CA 94114, USA
 print(location.address)
 # 3995 23rd St, San Francisco, CA 94114, USA
+print(location.altitude)
+# 0.0
 print((location.latitude, location.longitude))
-# (37.7529353, -122.4317224)
+# (37.7529648, -122.4317141)
 print(location.point)
-# 37 45m 10.5671s N, 122 25m 54.2006s W
+# 37 45m 10.6733s N, 122 25m 54.1708s W
 print(location.raw)
 # {'address_components': [
 #     {'long_name': '3995', 'short_name': '3995', 'types': ['street_number']},
