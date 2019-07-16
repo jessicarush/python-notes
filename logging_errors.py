0455674661c62c0cb3aa04b3d0b54183759c1b91
jessicarush <hello@cyan.red>
2019-07-16 12:55:18 -0700

    Docs: minor improvements

--- logging_errors.py
+++ logging_errors.py
@@ -7,7 +7,7 @@
 # The logging module includes:
 # – The message that you want to save to the log
 # – Ranked priority levels as functions:
-#   debug(), info(), warn(), error(), and critical()
+#   debug(), info(), warning(), error(), and critical()
 # – One or more logger objects as the main connection with the module
 # – Handlers that direct the message to your terminal, a file, a database,
 #   or somewhere else
@@ -17,14 +17,15 @@
 import logging
 
 fmt = '%(asctime)s %(levelname)s %(lineno)s %(message)s'
-logging.basicConfig(level=logging.DEBUG, filename='test.log', format=fmt)
+logging.basicConfig(level=logging.DEBUG, filename='data/test.log', format=fmt)
 
 # priority levels:
 logging.debug('debug message')
 logging.info('info message')
-logging.warn('warning message')
+logging.warning('warning message')
 logging.error('error message')
 logging.critical('critical message')
+
 
 # Breakdown
 # -----------------------------------------------------------------------------
@@ -35,12 +36,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 # handlers direct messages to different places, for example, a log file:
 
-logging.basicConfig(level=logging.DEBUG, filename='test.log')
+logging.basicConfig(level=logging.DEBUG, filename='data/test.log')
 
 # you can format your logged messages:
 
 fmt = '%(asctime)s %(levelname)s %(lineno)s %(message)s'
-logging.basicConfig(level=logging.DEBUG, filename='test.log', format=fmt)
+logging.basicConfig(level=logging.DEBUG, filename='data/test.log', format=fmt)
 
 # here's a logger object:
 
@@ -54,3 +55,43 @@ logger.debug('here is my debug message')
 # The information here is pretty scant. For more information on this topic, go
 # through the following in Doug Hellman's Python 3 Standard Library by example:
 # p.980–986, p.563–564, p.644–647, p.650–653, p.671–674
+
+
+# Log Record attributes
+# -----------------------------------------------------------------------------
+# There are a number of attributes (bits of information) that can be included
+# in your logged message format. For a full list see:
+# https://docs.python.org/3/library/logging.html#logrecord-attributes
+
+
+# Logging Levels
+# -----------------------------------------------------------------------------
+# The predifined logging levels all have a numeric value associated to them.
+# This is mostly of interest because you can define your own levels.
+
+# Level     | Numeric value
+# ----------|--------------
+# CRITICAL  | 50
+# ERROR     | 40
+# WARNING   | 30
+# INFO      | 20
+# DEBUG     | 10
+# NOTSET    | 0
+
+# To define your own level:
+
+SPECIAL_INFO_LEVEL_NUM = 21
+logging.addLevelName(SPECIAL_INFO_LEVEL_NUM, 'SPECIAL_INFO')
+
+def special_info(self, message, *args, **kws):
+    if self.isEnabledFor(SPECIAL_INFO_LEVEL_NUM):
+        self._log(SPECIAL_INFO_LEVEL_NUM, message, args, **kws)
+
+logging.Logger.special_info = special_info
+
+logger.special_info('Hey, this is special')
+
+# That being said, while defining your own levels is possible, it shouldn't
+# really be necessary, as the existing levels have been chosen on the basis of
+# practical experience. It's also a very bad idea if you are developing a
+# library as your custom level could conflict with others.
