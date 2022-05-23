3a42addda6065287b4d34a3be00cb80bbb062be7
jessicarush <jessica@13down.io>
2022-05-23 12:33:49 -0700

    Style: minor formatting

--- iterables_summary.py
+++ iterables_summary.py
@@ -15,11 +15,13 @@ colours_set = {'red', 'orange', 'black'}
 # Frozensets : immutable, unordered, values must be unique
 colours_fset = frozenset(('red', 'orange', 'black'))
 
-# Dict keys: immutable, unordered, values must be unique
-# Dict values: mutable, unordered, values don't need to be unique
-colours_dict = {'red': 'Pantone 185C',
-                'orange': 'Pantone 021C',
-                'black': 'Pantone 6C'}
+# Dict keys: immutable, ordered (as of Python 3.6), values must be unique
+# Dict values: mutable, ordered (as of Python 3.6), values don't need to be unique
+colours_dict = {
+    'red': 'Pantone 185C',
+    'orange': 'Pantone 021C',
+    'black': 'Pantone 6C'
+    }
 
 print(type(colours_list))   # <class 'list'>
 print(type(colours_tuple))  # <class 'tuple'>
@@ -35,27 +37,31 @@ print(colours_tuple[2])       # black
 print(colours_dict['black'])  # Pantone 6C
 
 # You can combine data structures, for example, you can make:
-#  - a tuple of lists
-#  - a list of lists
-#  - a dictionary of lists (only the values van be lists)
+# - a tuple of lists
+# - a list of lists
+# - a dictionary of lists (only the values van be lists)
 
 colors = ['magenta', 'red', 'cyan']
 moods = ['happy', 'sad', 'confused']
 senses = ['smell', 'touch', 'taste']
 
-dict_of_lists = {'Colors': colors,
-                 'Moods': moods,
-                 'Senses': senses,}
+dict_of_lists = {
+    'Colors': colors,
+    'Moods': moods,
+    'Senses': senses
+    }
 
 # Reminder: Dictionary keys are immutable, therefor you cannot use a list
 # or another dictionary as a key, but you CAN use a tuple or a frozenset
 # because those are immutable too. A good example of this is with mapping –
 # the GPS coordinates may be the key:
 
-places = {(44, -93, 344): 'home',
-          (27, -80, 200): 'work',}
+places = {
+    (44, -93, 344): 'home',
+    (27, -80, 200): 'work'
+    }
 
 # Note that these aren't the only iterables in Python. Other iterables include:
-#    - strings
-#    - generators
-#    - pandas series objects
+# - strings
+# - generators
+# - pandas series objects
